bc93a25f0def469a85f5608b5c8a5832d3459bcf
Romain Baugue <rb@capitaldata.fr>
2015-03-04 15:51:22 +0100

    Added packer configuration for an archlinux32 base box

---
+++ archlinux32/install.sh
@@ -0,0 +1,86 @@
+#! /bin/bash
+
+DISK="/dev/sda"
+PARTITION_NUMBER="1"
+PARTITION="$DISK$PARTITION_NUMBER"
+MOUNTPOINT="/mnt"
+FQDN="vagrant"
+TIMEZONE="Europe/Paris"
+LANGUAGE="en_US.UTF-8"
+KEYMAP="fr"
+
+#
+# Partitions
+#
+
+# Remove old partition table
+sgdisk --zap $DISK 
+
+# Create a new partition from start to end of the available space
+sgdisk --new=$PARTITION_NUMBER:0:0 $DISK 
+
+# Make partition bootable by setting it's 2nd bit up
+sgdisk --attribute=$PARTITION_NUMBER:set:2
+
+# Create the ext4 filesystem
+mkfs -t ext4 ${PARTITION}
+
+# Mount the partition
+mount ${PARTITION} $MOUNTPOINT
+
+
+#
+# System
+#
+
+# Bootstrap the system
+pacstrap $MOUNTPOINT base base-devel
+
+# Generate the filesystem table
+genfstab -U -p $MOUNTPOINT >> $MOUNTPOINT/etc/fstab
+
+
+#
+# Setup
+#
+
+# Hostname and locales
+arch-chroot $MOUNTPOINT echo $FQDN > /etc/hostname
+arch-chroot $MOUNTPOINT ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+arch-chroot $MOUNTPOINT echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+arch-chroot $MOUNTPOINT sed -i "s/#$LANGUAGE/$LANGUAGE/" /etc/locale.gen
+arch-chroot $MOUNTPOINT echo "LANG=$LANGUAGE" > /etc/locale.conf
+arch-chroot $MOUNTPOINT locale-gen
+
+# Enable networking on boot
+arch-chroot $MOUNTPOINT systemctl enable dhcpcd
+
+# Initial RAM disk
+arch-chroot $MOUNTPOINT mkinitcpio -p linux
+
+# Install and configure the bootloader
+arch-chroot $MOUNTPOINT pacman -S --noconfirm gptfdisk syslinux
+arch-chroot $MOUNTPOINT syslinux-install_update -i -a -m
+arch-chroot $MOUNTPOINT sed -i 's/sda3/sda1/' /boot/syslinux/syslinux.cfg
+
+# Install and configure openssh
+arch-chroot $MOUNTPOINT pacman -S --noconfirm openssh
+arch-chroot $MOUNTPOINT systemctl enable sshd
+arch-chroot $MOUNTPOINT sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/' /etc/ssh/sshd_config
+arch-chroot $MOUNTPOINT sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+arch-chroot $MOUNTPOINT sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords yes/' /etc/ssh/sshd_config
+
+
+#
+# Cleanup
+#
+
+arch-chroot pacman -Rcns --noconfirm gptfdisk
+arch-chroot pacman -Scc --noconfirm
+
+
+#
+# Reboot
+#
+
+reboot now
---
+++ archlinux32/provision.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+# Virtualbox Guest Additions
+pacman -S --noconfirm linux-headers virtualbox-guest-utils 
+systemctl enable vboxservice
+
+# Users
+groupadd vagrant
+useradd --comment "Vagrant" --create-home --gid users --groups vagrant,vboxsf vagrant
+
+# Passwords
+passwd <<EOI
+vagrant
+vagrant
+EOI
+
+passwd vagrant <<EOI
+vagrant
+vagrant
+EOI
+
+# Sudo
+pacman -S --noconfirm sudo
+echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_vagrant
+echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_vagrant
+chmod 0440 /etc/sudoers.d/10_vagrant
+
+# SSH
+mkdir /home/vagrant/.ssh
+curl --output /home/vagrant/.ssh/authorized_keys --location https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+chown vagrant:users -R /home/vagrant/.ssh
+chmod 0700 /home/vagrant/.ssh
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+
+# NFS
+pacman -S --noconfirm net-tools nfs-utils
+systemctl enable rpcbind nfs-server nfs-client.target
+
+# Utils
+pacman -S --noconfirm htop tmux
+
+# Cleanup
+
+pacman -Scc --noconfirm
