d327c8a5f8482878f63d0ed33e46815985c2baef
Romain Baugue <rb@capitaldata.fr>
2015-03-11 15:42:57 +0100

    Boyscouting

--- provision-php.sh
+++ provision-php.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-pacman -S --noconfirm php php-fpm php-mcrypt
+pacman -S --noconfirm php php-fpm php-mcrypt php-gd
 systemctl enable php-fpm
 
 sed -i 's/display_errors = Off/display_errors = On/' /etc/php/php.ini
 sed -ri 's#open_basedir = (.*)#open_basedir = /#' /etc/php/php.ini
-sed -ri 's/;extension=(phar|openssl|mcrypt).so/extension=\1.so/' /etc/php/php.ini
+sed -ri 's/;extension=(phar|openssl|mcrypt|zip|gd).so/extension=\1.so/' /etc/php/php.ini
 
 sed -i 's/listen.owner = http/listen.owner = vagrant/' /etc/php/php-fpm.conf
 sed -i 's/listen.group = http/listen.group = vagrant/' /etc/php/php-fpm.conf 
