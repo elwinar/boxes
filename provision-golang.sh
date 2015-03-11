df230a5994893a60f7044b7199079ea3232957bd
Romain Baugue <rb@capitaldata.fr>
2015-03-11 15:42:35 +0100

    Add the golang box

--- provision-golang.sh
+++ provision-golang.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-pacman -S --noconfirm go
+pacman -S --noconfirm go git subversion mercurial
 echo "export GOPATH=~
 export PATH=\$GOPATH/bin:\$PATH
-" > ~/.bashrc
+" >> ~/.bashrc
+source ~/.bashrc
+go get golang.org/x/tools/cmd/...
