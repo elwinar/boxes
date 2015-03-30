ed040b84b16bbed9d2130f257832b7a05cbfd139
Romain Baugue <rb@capitaldata.fr>
2015-03-30 08:14:54 +0200

    Fix composer and golang install

--- provision-composer.sh
+++ provision-composer.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer
 pacman -S --noconfirm git
--- provision-golang.sh
+++ provision-golang.sh
@@ -3,6 +3,4 @@
 pacman -S --noconfirm go git subversion mercurial
 echo "export GOPATH=~
 export PATH=\$GOPATH/bin:\$PATH
-" >> ~/.bashrc
-source ~/.bashrc
-go get golang.org/x/tools/cmd/...
+" >> /home/vagrant/.bashrc
