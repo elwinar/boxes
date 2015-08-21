82d3ccdb20275d1481fbebd26f58554865d7f01c
Romain Baugue <romain.baugue@elwinar.com>
2015-08-21 14:00:34 +0200

    Moved files around

--- provisioners/golang.sh
+++ provisioners/golang.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-pacman -S --noconfirm go git subversion mercurial
+pacman -S --noconfirm go git subversion mercurial bzr
 echo "export GOPATH=~
 export PATH=\$GOPATH/bin:\$PATH
 " >> /home/vagrant/.bashrc
